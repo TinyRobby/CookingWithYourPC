833101cba15816b5ef0b3949b985bdc6abc7b58d
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-04-12 21:35:45 +0000

    qqqq

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main() 
 {
-  cout << "Hello World!\n";
+  
+	cout << "Hello! Welcome to \"Cooking with your PC\"!\n\\h for help, \\s to start!";
+	string cwp;
+  	bool game = true;
+  	while(game == true)
+  	{
+		cin >> cwp;
+		if(cwp == "\\h") {
+			cout << "Basic commands:\n\\h this\n\\s Start the game" << endl;
+		}
+		
+  	}
 }
